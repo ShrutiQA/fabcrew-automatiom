c9c875676129559acf968b16a7634102ece71a4d
Shruti Kumari Karna <shrutikarna1999@gmail.com>
2026-08-05 11:33:49 +0545

    Added allure reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,12 +10,17 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+  ['html'],
+  ['allure-playwright'],
+],
 
   use: {
-    baseURL: process.env.BASE_URL,
-    trace: 'on-first-retry',
-  },
+  baseURL: process.env.BASE_URL,
+  trace: 'on-first-retry',
+  screenshot: 'only-on-failure',   
+  video: 'retain-on-failure',       
+},
 
   projects: [
     {
